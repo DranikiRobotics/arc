3328826f17d98d4fd0879878555f005cfc5169d1
AtomicGamer9523 <drunkenmonkey4987@gmail.com>
2023-11-11 07:59:49 -0800

    + cargo binstall

--- libs/scripts.py
+++ libs/scripts.py
@@ -15,6 +15,20 @@ def fmt(cd: str, args: list[str]) -> str | int | None:
     if res.returncode != 0: exit(res.returncode)
 
 def test(cd: str, args: list[str]) -> str | int | None:
+    # Check if tarpaulin is installed
+    def is_tarpaulin_installed() -> bool:
+        res = subprocess.run(["cargo", "tarpaulin", "--version"], capture_output=True)
+        return res.returncode == 0
+    
+    # Install tarpaulin
+    def install_tarpaulin() -> None:
+        res = subprocess.run(["cargo", "binstall", "cargo-tarpaulin"])
+        if res.returncode != 0: exit(res.returncode)
+    
+    # Check if tarpaulin is installed and install it if it isn't
+    if not is_tarpaulin_installed(): install_tarpaulin()
+
+    # Run tarpaulin
     cmd = ["cargo", "tarpaulin"]
     cmd.extend(args)
     res = subprocess.run(cmd, cwd=cd)
