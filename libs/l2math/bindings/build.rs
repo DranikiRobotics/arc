4cee85ba3c44f151402f546803cd3a6748cfb97f
AtomicGamer9523 <drunkenmonkey4987@gmail.com>
2023-11-23 12:11:44 -0800

    Removed debug print statements

--- libs/l2math/bindings/build.rs
+++ libs/l2math/bindings/build.rs
@@ -10,17 +10,10 @@ fn android() {
     println!("cargo:rustc-link-lib=c++_shared");
 
     if let Ok(output_path) = env::var("CARGO_NDK_OUTPUT_PATH") {
-        println!("output_path: {}", &output_path);
         let sysroot_libs_path =
             PathBuf::from(env::var_os("CARGO_NDK_SYSROOT_LIBS_PATH").unwrap());
         let lib_path = sysroot_libs_path.join("libc++_shared.so");
-        let metadata = std::fs::metadata(&lib_path).unwrap();
-        println!("metadata: {:?}", &metadata);
-        std::fs::copy(
-            lib_path,
-            Path::new(&output_path)
-                .join("libc++_shared.so"),
-        )
-        .unwrap();
+        let new_lib_path = Path::new(&output_path).join("libc++_shared.so");
+        std::fs::copy(lib_path, new_lib_path).unwrap();
     }
 }
