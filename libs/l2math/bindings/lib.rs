4ffd51c30446b59bb8ee330bd299537480eb9a83
AtomicGamer9523 <drunkenmonkey4987@gmail.com>
2023-11-22 18:08:07 -0800

    fmt bypass ;)

--- libs/l2math/bindings/lib.rs
+++ libs/l2math/bindings/lib.rs
@@ -6,7 +6,8 @@
 pub use l2math::*;
 
 #[doc(hidden)]
+#[rustfmt::skip]
 #[warn(dead_code)]
 #[allow(non_snake_case)]
 #[cfg(not(feature = "no_std"))]
-fn Relax_this_is_not_an_actual_warning_This_is_here_to_inform_you_that_this_crate_is_not_being_built_with_no_std_() {}
+fn Relax_this_is_not_an_actual_warning_This_is_here_to_inform_you_that_this_crate_is_not_being_built_with_no_std_(){}
