b3b3f21df184643af858b2be4e6144c719efb0e4
AtomicGamer9523 <drunkenmonkey4987@gmail.com>
2023-11-22 18:06:19 -0800

    2 fixes

--- libs/l2math/bindings/lib.rs
+++ libs/l2math/bindings/lib.rs
@@ -4,3 +4,9 @@
 
 #[doc(hidden)]
 pub use l2math::*;
+
+#[doc(hidden)]
+#[warn(dead_code)]
+#[allow(non_snake_case)]
+#[cfg(not(feature = "no_std"))]
+fn Relax_this_is_not_an_actual_warning_This_is_here_to_inform_you_that_this_crate_is_not_being_built_with_no_std_() {}
