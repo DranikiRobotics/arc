5ea09f261e2358d716e40d9b743527345c9c537d
AtomicGamer9523 <drunkenmonkey4987@gmail.com>
2023-11-24 11:17:30 -0800

    Now ?

--- libs/l2math/bindings/build.rs
+++ libs/l2math/bindings/build.rs
@@ -1,3 +1,5 @@
+#![allow(unused)]
+
 use std::env;
 use std::path::{Path, PathBuf};
 
--- libs/scripts/build.py
+++ libs/scripts/build.py
@@ -4,10 +4,11 @@ def build(cd: str, args: list[str]) -> str | int | None:
     cmd = ["cargo", "build"]
 
     if "--android" in args:
-        args.remove("--android")
-        cmd.extend(["--target", "aarch64-linux-android"])
-        cmd.extend(["--features", "__android__"])
-
+        args.remove("--android") # Remove the flag
+        cmd.extend(["--target", "aarch64-linux-android"]) # Set the target
+        cmd.extend(["--config", "target.aarch64-linux-android.linker='aarch64-linux-android-cc'"]) # Set the linker
+        cmd.extend(["--features", "__android__"]) # Set the features
+    
     cmd.extend(args)
     res = subprocess.run(cmd, cwd=cd)
     if res.returncode != 0: exit(res.returncode)
