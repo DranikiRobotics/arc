5279fd7559cfaa97536674eeb81aef53fe664f46
AtomicGamer9523 <drunkenmonkey4987@gmail.com>
2023-11-24 11:03:03 -0800

    Turns out it wasn't the NDK failing

--- libs/scripts/build.py
+++ libs/scripts/build.py
@@ -2,6 +2,12 @@ def build(cd: str, args: list[str]) -> str | int | None:
     import subprocess
     
     cmd = ["cargo", "build"]
+
+    if "--android" in args:
+        args.remove("--android")
+        cmd.extend(["--target", "aarch64-linux-android"])
+        cmd.extend(["--features", "__android__"])
+
     cmd.extend(args)
     res = subprocess.run(cmd, cwd=cd)
     if res.returncode != 0: exit(res.returncode)
