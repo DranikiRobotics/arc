5d0e4d0c31e621baca86e6a802e1a24a8407581c
AtomicGamer9523 <drunkenmonkey4987@gmail.com>
2023-12-14 07:30:26 -0800

    Another run improvement

--- arc/bin/main.rs
+++ arc/bin/main.rs
@@ -1,3 +1,5 @@
 fn main() {
+    let args: Vec<String> = std::env::args().collect();
     println!("ARC");
+    println!("args: {:?}", args);
 }
--- libs/scripts/run.py
+++ libs/scripts/run.py
@@ -2,11 +2,13 @@ def run(cd: str, args: list[str]) -> str | int | None:
     import subprocess
 
     binary = "arc"
-    if len(args) > 0:
-        if '--bin=' in args[0]:
-            binary = args[0].split('=')[1]
-            args = args[1:]
+    for i in range(len(args)):
+        arg = args[i]
+        if '--bin=' in arg:
+            binary = arg.split('=')[1]
+            args.pop(i)
 
     cmd = ["cargo", "run", "--bin", binary]
+    cmd.extend(args)
     res = subprocess.run(cmd, cwd=cd)
     if res.returncode != 0: return res.returncode
