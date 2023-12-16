007ec53aa264a9a67c9869323187b55d2b63e98a
AtomicGamer9523 <drunkenmonkey4987@gmail.com>
2023-12-16 07:25:46 -0800

    Fixed CI and build.py

--- libs/scripts/build.py
+++ libs/scripts/build.py
@@ -1,6 +1,11 @@
 import subprocess, os
 
 def build(cd: str, args: list[str]) -> str | int | None:
+    if "--android" in args:
+        print("Android builds are currently halted.")
+        print("Hopefully they will be back soon.")
+        return 0
+
     cmd = ["cargo", "build"]
     cmd.extend(args)
     res = subprocess.run(cmd, cwd=cd)
